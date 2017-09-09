938c1b9aa60b1bf007a7f477e802e7ca251b85c7
jonmigueljara <jmjara93@gmail.com>
2017-09-09 15:58:27 -0700

    removed random text.

--- examples/SingleLetterDisplaySimple.java
+++ examples/SingleLetterDisplaySimple.java
@@ -65,7 +65,6 @@ public class SingleLetterDisplaySimple extends Application {
                     displayText.setText(characterTyped);
                     keyEvent.consume();
 
-                    gergnrn
                 }
 
                centerText();
